2618052967ed46014fd3214a5ffe1b4759bfc276
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-09-11 12:53:54 +0300

    FINALLY WORKING WOW WOW WOW

---
+++ main.py
@@ -0,0 +1,60 @@
+import pygame as pg
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+cubeVertices = ((1, 1, 1), (1, 1, -1), (1, -1, -1), (1, -1, 1), (-1, 1, 1), (-1, -1, -1), (-1, -1, 1), (-1, 1, -1))
+cubeEdges = ((0, 1), (0, 3), (0, 4), (1, 2), (1, 7), (2, 5), (2, 3), (3, 6), (4, 6), (4, 7), (5, 6), (5, 7))
+cubeQuads = ((0, 3, 6, 4), (2, 5, 6, 3), (1, 2, 5, 7), (1, 0, 4, 7), (7, 4, 6, 5), (2, 3, 0, 1))
+
+
+def aga():
+    glBegin(GL_POINTS)
+    glVertex2f(0, 0)
+    glVertex2f(0.05, 0)
+    glEnd()
+
+
+def wireCube():
+    glBegin(GL_TRIANGLES)
+    for cubeEdge in cubeEdges:
+        for cubeVertex in cubeEdge:
+            glVertex3fv(cubeVertices[cubeVertex])
+    glEnd()
+
+
+def solidCube():
+    glBegin(GL_QUADS)
+    for cubeQuad in cubeQuads:
+        for cubeVertex in cubeQuad:
+            glVertex3fv(cubeVertices[cubeVertex])
+    glEnd()
+
+
+def main():
+    pg.init()
+    display = (800, 800)
+    pg.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0, 0.0, -5)
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+
+        # glRotatef(1, 1, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        solidCube()
+        # aga()
+        pg.display.flip()
+        # pg.time.wait(10)
+
+
+if __name__ == "__main__":
+    main()
